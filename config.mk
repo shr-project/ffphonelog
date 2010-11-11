79a3a468ccbad5255710c2da1b80eb46f1b041dc
Łukasz Pankowski <lukpank@o2.pl>
2010-11-11 13:24:47 +0100

    bump DISTRO_PR to much SHR

--- config.mk
+++ config.mk
@@ -12,6 +12,7 @@ NEO=192.168.0.202
 # can be autodetected with: make autodetect-options
 BASE_PACKAGE_ARCH=armv4t
 DEPLOY_DIR_IPK=${OE_TOPDIR}/tmp/deploy/ipk
+DISTRO_PR=.6
 
-IPK_BASENAME=ffphonelog_${VERSION}-r0.5_${BASE_PACKAGE_ARCH}.ipk
+IPK_BASENAME=ffphonelog_${VERSION}-r0${DISTRO_PR}_${BASE_PACKAGE_ARCH}.ipk
 IPK_DIR=${DEPLOY_DIR_IPK}/${BASE_PACKAGE_ARCH}
